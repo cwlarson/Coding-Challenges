fd1603b2160b032067a9cf653479858604b1be66
Casey W. Larson <caseywlarson@gmail.com>
2017-02-27 19:26:14 -0800

    Update code-sample.js

--- AlignCommerce/code-sample.js
+++ AlignCommerce/code-sample.js
@@ -13,14 +13,28 @@
   }
 
   let body = document.getElementsByTagName('body')[0];
-  function changeColor() {
+
+  /*
+   * @param {number} frequency: frequency of color change (in milliseconds)
+   * @param {number} cycles: total number of times that the color should change 
+   */
+  function changeColor(frequency, cycles) {
+    // Default to 1s frequency and 10 cycles
+    frequency = frequency || 1000;
+    cycles = cycles || 10;
+    
+    // Change background color
     let newColor = getNewRandomColor();
     body.style.backgroundColor = `rgb(${newColor})`;
     colorsUsed[newColor] = true;
 
     if(Object.keys(colorsUsed).length < 10) {
-      setTimeout(changeColor, 1000);
+      setTimeout(() => {
+        changeColor(frequency, cycles);
+      }, 1000);
     }
   }
   changeColor();
+  // changeColor(500);
+  // changeColor(1000, 25);
 })();
